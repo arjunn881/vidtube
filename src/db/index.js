50228067e6792a0f360572edfe5bf17ba2160653
arjunn881 <arjunn881@gmail.com>
2024-09-19 20:55:22 +0530

    19092024

--- src/db/index.js
+++ src/db/index.js
@@ -3,12 +3,9 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
-    console.log(`${process.env.MONGO_DB}/${DB_NAME}`)
     await mongoose.connect(`${process.env.MONGO_DB}/${DB_NAME}`);
-    
-    console.log(
-      `\n MongoDB connected ! DB HOST : ${connectionInstance.connection.host}`
-    );
+
+    console.log(`\n MongoDB connected ! DB HOST `);
   } catch (error) {
     console.log("MongoDB CONNECTION ERROR !!!", error);
     process.exit(1);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import { app } from "./app.js";
 import  dotenv  from "dotenv";
+// import 'dotenv/config'
+import { app } from "./app.js";
+
 import connectDB from "./db/index.js";
+
+
 
 dotenv.config({
     path:"./.env"
 })
 
 
-const PORT = process.env.PORT ||5000;
+const PORT = process.env.PORT ||5001;
 
-console.log(process.env.PORT)
+// -r dotenv/config --experimental-json-modules
 
 connectDB()
 .then(()=>{
