e55b185a83c5bd5ab1c53e22149ae912aa144419
arjunn881 <arjunn881@gmail.com>
2024-09-19 21:49:15 +0530

    3

--- src/app.js
+++ src/app.js
@@ -10,14 +10,15 @@ app.use(
   })
 );
 
-
 //common middleware
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
+//Import Routes
+import healthcheckRouter from "./routes/healthcheck.routes.js";
 
-
-
+//Routes
+app.use("/api/v1/healthcheck", healthcheckRouter);
 
 export { app };
