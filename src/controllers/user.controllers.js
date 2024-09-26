fe36840946d2d1aac8baf44c1210ad752e9e53e3
arjunn881 <arjunn881@gmail.com>
2024-09-26 23:55:57 +0530

    updated

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -4,6 +4,24 @@ import { User } from "../models/user.models.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   const { fullName, email, username, password } = req.body;
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw ApiError(400, "All fiels are requird!");
+  }
 });
+
+const existingUser = await User.findOne({
+  $or :[{username}, {email}]
+})
+
+if(existingUser){
+  throw ApiError(409, "User with eail or username already existed!")
+}
+
+const avatarLocalPath = req.files?.avatar[0]?.path
+const coverLocalPath = req.files?.coverImage[0]?.path
+
+
 
 export { registerUser };
